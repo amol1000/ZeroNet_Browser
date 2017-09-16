ddea1ddfd59a79caf086d4794d1fde9400622394
amol <amol1000.ak@gmail.com>
2017-09-16 21:36:28 +0530

    added figlet in build.sh

--- Build.sh
+++ Build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install figlet
+
+figlet -f small "Installing Dependencies"
+
 sudo apt-get install python-webkit-dev
 
 sudo apt-get install python-pip
@@ -13,3 +17,5 @@ cd ZeroNet_Browser
 wget https://github.com/amol1000/ZeroNet_Browser/archive/master.zip
 
 unzip master.zip
+
+figlet -f small "ZERONET BORWSER INSTALLED"
